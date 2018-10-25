b2f516985ad4811974e7b40d74ffeb0b97f8d20e
GregP <37701095+GregPyemont@users.noreply.github.com>
2018-10-25 21:36:03 +0100

    retrying upload

---
+++ docs/bt.js
@@ -0,0 +1,11 @@
+function clicky() {
+    var x = document.getElementById("mu");
+	
+    if (x.style.display === "block") {
+        x.style.display = "none";
+		
+    }
+	else {
+        x.style.display = "block";
+    }
+}
